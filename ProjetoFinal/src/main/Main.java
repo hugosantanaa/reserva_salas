7c2b3003863aa5235731a125290c5793b034dbf1
hugo santana <hugo.s2005@hotmail.com>
2023-04-05 15:37:58 -0300

    Versão 2.0

---
+++ ProjetoFinal/src/main/Main.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import View.Login_GUI;
+
+
+public class Main {
+
+ 
+    
+    public static void main(String[] args) {
+        new Login_GUI().setVisible(true);
+        
+    }
+    
+}
